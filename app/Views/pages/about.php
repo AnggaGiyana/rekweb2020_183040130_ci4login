f9ad8134f8b2474f687a64c4ad78f38951a84270
AnggaGiyana <putra.183040130@mail.unpas.ac.id>
2020-10-28 17:02:07 +0700

    menambahkan fitur login

---
+++ app/Views/pages/about.php
@@ -0,0 +1,12 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id, vel eligendi. Dolorum quasi qui rem tenetur reiciendis amet repudiandae vero deserunt explicabo neque unde nisi, porro suscipit quisquam quis officiis!</p>
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
